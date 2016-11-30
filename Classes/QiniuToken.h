e9553c4ab2f89e88979057f6cbb82ff594e61eaf
CodeEagle <stasura@hotmail.com>
2016-11-30 11:09:59 +0800

    fixed for framework

--- Classes/QiniuToken.h
+++ Classes/QiniuToken.h
@@ -43,18 +43,18 @@
 
 /**
  *  initialize instance with scope, secret key and access key, the defaut live time is 5 minutes.
- *  @param scope scope is a name of cloud storage.
- *  @param SecretKey secretKey of cloud storage.
- *  @param Accesskey accessKey of cloud storage.
+ *  @param theScope scope is a name of cloud storage.
+ *  @param theSecretKey secretKey of cloud storage.
+ *  @param theAccessKey accessKey of cloud storage.
  */
 + (id)registerWithScope:(NSString *)theScope SecretKey:(NSString*)theSecretKey Accesskey:(NSString*)theAccessKey;
 
 
 /**
  *  initialize instance with scope, secret key and access key.
- *  @param scope scope is a name of cloud storage.
- *  @param SecretKey secretKey of cloud storage.
- *  @param Accesskey accessKey of cloud storage.
+ *  @param theScope scope is a name of cloud storage.
+ *  @param theSecretKey secretKey of cloud storage.
+ *  @param theAccessKey accessKey of cloud storage.
  *  @param theliveTime the time to live of token.
  */
 
---
+++ QiniuUploadDemo/Qiniu/Qiniu.h
@@ -0,0 +1,37 @@
+//
+//  Qiniu.h
+//  Qiniu
+//
+//  Created by LawLincoln on 2016/11/30.
+//  Copyright © 2016年 胡 桓铭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Qiniu.
+FOUNDATION_EXPORT double QiniuVersionNumber;
+
+//! Project version string for Qiniu.
+FOUNDATION_EXPORT const unsigned char QiniuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Qiniu/PublicHeader.h>
+
+
+#import <Qiniu/ALAssetsLibrary+POS.h>
+#import <Qiniu/NSInputStream+POS.h>
+#import <Qiniu/POSAdjustedAssetReaderIOS7.h>
+#import <Qiniu/POSAdjustedAssetReaderIOS8.h>
+#import <Qiniu/POSAssetReader.h>
+#import <Qiniu/POSBlobInputStream.h>
+#import <Qiniu/POSBlobInputStreamAssetDataSource.h>
+#import <Qiniu/POSBlobInputStreamDataSource.h>
+#import <Qiniu/POSFastAssetReader.h>
+#import <Qiniu/POSInputStreamLibrary.h>
+#import <Qiniu/POSLocking.h>
+#import <Qiniu/QUMDefines.h>
+#import <Qiniu/QiniuFile.h>
+#import <Qiniu/QiniuToken.h>
+#import <Qiniu/QiniuMultipartElement.h>
+#import <Qiniu/QiniuInputStream.h>
+#import <Qiniu/QiniuUploader.h>
+#import <Qiniu/QUMBase64.h>
